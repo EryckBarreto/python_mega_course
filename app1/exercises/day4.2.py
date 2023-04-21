d9d412bb9671bfb0631c8ac78997d8857ad56a2b
Eryck Barreto <eryck.barreto@hotmail.com>
2023-04-21 19:32:07 -0300

    Exercício de fixação de uso de iteração e lista
    O objetivo era colocar o lugar do rank e sair o nome da pessoa que estava em primeiro, segundo ou terceiro lugar

---
+++ app1/exercises/day4.2.py
@@ -0,0 +1,18 @@
+# ranking = ['John', 'Sen', 'Lisa']
+# rank = int(input("Enter a rank number: "))
+# i = rank - 1
+# print(ranking[i])
+
+# ----------------------------
+
+# ranking = ['John', 'Sen', 'Lisa']
+
+# rank = int(input("Enter rank number: ")) - 1
+# print(ranking[rank])
+
+# ---------------------------------
+
+ranking = ['John', 'Sen', 'Lisa']
+rank = int(input("Enter rank number:" ))
+name = ranking[rank]
+print(name)
