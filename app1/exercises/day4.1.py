a1a970330d3ed043d060d6823e10c842da8c912f
Eryck Barreto <eryck.barreto@hotmail.com>
2023-04-21 19:30:45 -0300

    Exercício de fixação do uso do float para alterar a string

---
+++ app1/exercises/day4.1.py
@@ -0,0 +1,10 @@
+# dollar = input("How many dollars have you got? ")
+# dollar = float(dollar)
+# euro = dollar * 0.95
+
+# print(f'The amount in euros is: {euro}')
+
+rate = 0.95
+dollars = float(input("How many dollars have you got? "))
+euros = dollars * rate
+print(f'The amount in euros is: {euros}')
